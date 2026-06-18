1d78691523151c5048735fb57ddca5a13e3f163c
Arturo Bernal <arturober@gmail.com>
2026-06-18 12:32:35 +0200

    Ejemplo tipos de datos

---
+++ Programación básica/01 Tipos de datos/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Me llamo Arturo");
+
+int num = 23; // Variable
+num = 13; // Reasignación de valor
+
+const double PI = 3.1416; // Constante. No se puede cambiar el valor
+
+Console.WriteLine(num); // 13
+
+int num2 = num / 3;
+Console.WriteLine(num2); // 4
+Console.WriteLine(num % 3); // 1
+Console.WriteLine(num / 3.0); // 4,33333333
+
+string nombre = "Pepe";
+Console.WriteLine(nombre); // Pepe
+Console.WriteLine("Hola " + nombre); // Pepe
+
+double precio = 2.32;
+Console.WriteLine(precio + "€");
+
+bool cierto = false;
+Console.WriteLine("¿Es cierto?: " + cierto);
