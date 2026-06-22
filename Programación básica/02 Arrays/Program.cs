6d865ff257d6e802cdc569fa8201e846127e35f6
Fernando Ruiz <fernando@iessanvicente.com>
2026-06-22 12:28:40 +0200

    Añadir implementación de matriz 3x3 y bucles anidados para recorrerla, además de manejo de entrada para el día de la semana.

--- Programación básica/02 Arrays/Program.cs
+++ Programación básica/02 Arrays/Program.cs
@@ -35,3 +35,46 @@ Console.WriteLine(precios.Length);  // 4
 Console.WriteLine(respuestas.Length);  // 4
 Console.WriteLine(nombres[2]);  // María
 Console.WriteLine(precios[1]);  // 15.5
+
+// Declarar una matriz 3x3
+int[,] matriz = new int[3, 3];
+
+// Asignar valores
+matriz[0, 0] = 1; matriz[0, 1] = 2; matriz[0, 2] = 3;
+matriz[1, 0] = 4; matriz[1, 1] = 5; matriz[1, 2] = 6;
+matriz[2, 0] = 7; matriz[2, 1] = 8; matriz[2, 2] = 9;
+
+// Forma directa
+int[,] matriz2 = {
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+// Recorrer con bucles anidados
+int filas = matriz2.GetLength(0);     // 3 filas
+int columnas = matriz2.GetLength(1);  // 3 columnas
+
+Console.WriteLine("Filas: " + filas);
+Console.WriteLine("Columnas: " + columnas);
+for (int i = 0; i < filas; i++)
+{
+    for (int j = 0; j < columnas; j++)
+    {
+        Console.Write($"{matriz2[i, j],4}");
+    }
+    Console.WriteLine();
+}
+
+
+Console.Write("Introduce el día de la semana (0-6): ");
+int dia = int.Parse(Console.ReadLine()!);
+
+string[] diasSemana = { "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo" };
+
+while (dia < 0 || dia > 6)
+{
+    Console.Write("Número inválido. Introduce un número entre 0 y 6: ");
+    dia = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine($"El día de la semana es: {diasSemana[dia]}");
