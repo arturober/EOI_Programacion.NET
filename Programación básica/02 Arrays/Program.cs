df308d0445e21a0b58fc62a11d07b479ce9348fa
Fernando Ruiz <fernando@iessanvicente.com>
2026-06-22 12:05:45 +0200

    Añadir ejemplos de diferentes formas de declarar y utilizar arrays en C#

--- Programación básica/02 Arrays/Program.cs
+++ Programación básica/02 Arrays/Program.cs
@@ -14,4 +14,24 @@ numeros[4] = 17;
 Console.WriteLine(numeros[0]);  // 18
 Console.WriteLine(numeros[2]);  // 22
 Console.WriteLine(numeros[4]);  // 17   
-    
+
+// Forma larga
+int[] numeros2 = new int[] { 18, 20, 22, 45, 17 };
+
+// Forma abreviada (recomendada)
+int[] numeros3 = { 10, 20, 30, 40, 50 };
+
+// Forma moderna con new[] (el compilador infiere el tipo)
+var numeros4 = new[] { 10, 20, 30, 40, 50 };
+
+// Arrays de otros tipos
+string[] nombres = { "Ana", "Luis", "María", "Pedro" };
+double[] precios = { 9.99, 15.50, 3.25, 7.80 };
+bool[] respuestas = { true, false, true, true };
+
+Console.WriteLine(numeros.Length);  // 5
+Console.WriteLine(nombres.Length);  // 4
+Console.WriteLine(precios.Length);  // 4
+Console.WriteLine(respuestas.Length);  // 4
+Console.WriteLine(nombres[2]);  // María
+Console.WriteLine(precios[1]);  // 15.5
