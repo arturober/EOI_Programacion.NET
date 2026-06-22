f44d189553dba29321048246cbe6a7f0d0669823
Fernando Ruiz <fernando@iessanvicente.com>
2026-06-22 12:00:18 +0200

    Añadir implementación de un array de enteros y mostrar sus valores

--- Programación básica/02 Arrays/Program.cs
+++ Programación básica/02 Arrays/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Declarar un array de 5 enteros (todos valen 0 inicialmente)
+int[] numeros = new int[5];
+
+// Asignar valores individual por posición
+numeros[0] = 18;
+numeros[1] = 20;
+numeros[2] = 22;
+numeros[3] = 45;
+numeros[4] = 17;
+
+// Mostrar valores del array
+// Posición: [ 0,  1,  2,  3,  4]
+// Valor:    [18, 20, 22, 45, 17]
+Console.WriteLine(numeros[0]);  // 18
+Console.WriteLine(numeros[2]);  // 22
+Console.WriteLine(numeros[4]);  // 17   
+    
