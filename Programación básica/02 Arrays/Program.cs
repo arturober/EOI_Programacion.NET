0b6a9925964fa6ecb053043250a1e6f281f304e4
Fernando Ruiz <fernando@iessanvicente.com>
2026-06-23 07:38:34 +0200

    Añadir ejercicio para imprimir un tablero de 3x3 del juego Tres en Raya usando un array 2D

--- Programación básica/02 Arrays/Program.cs
+++ Programación básica/02 Arrays/Program.cs
@@ -80,3 +80,52 @@ while (dia < 0 || dia > 6)
     dia = int.Parse(Console.ReadLine()!);
 }
 Console.WriteLine($"El día de la semana es: {diasSemana[dia]}");
+
+
+// Ejercicio para practicar: Imprimir un tablero de 3x3 de un juego de Tres en Raya usando un array 2D.
+char[,] tresenraya = {
+    // 0    1     2  
+    { 'O', '-',  '-' }, // 0
+    { 'O', 'X',  '-' }, // 1
+    { 'O', '-',  'X' }  // 2
+};
+
+
+Console.WriteLine("Imprimimos las casilla del tablero una a una:");
+
+// Fila 0
+Console.Write(tresenraya[0, 0]); // Columna 0 // O
+Console.Write(tresenraya[0, 1]); // Columna 1 // -
+Console.Write(tresenraya[0, 2]); // Columna 2 // -
+
+Console.WriteLine(); // Salto de línea
+
+// Fila 1
+Console.Write(tresenraya[1, 0]); // Columna 0 // O
+Console.Write(tresenraya[1, 1]); // Columna 1 // X
+Console.Write(tresenraya[1, 2]); // Columna 2 // -
+
+Console.WriteLine(); // Salto de línea
+
+// Fila 2
+Console.Write(tresenraya[2, 0]); // Columna 0 // O
+Console.Write(tresenraya[2, 1]); // Columna 1 // -
+Console.Write(tresenraya[2, 2]); // Columna 2 // X
+
+Console.WriteLine(); // Salto de línea
+
+
+Console.WriteLine("Imprimimos todo el tablero con un bucle:");
+
+for (int i = 0; i < 3; i++) // 3 filas
+{
+    for (int j = 0; j < 3; j++) // 3 columnas
+    {
+        Console.Write(tresenraya[i, j]); // Imprime cada casilla
+    }
+    Console.WriteLine();
+}
+
+// O - - 
+// O X - 
+// O - X
