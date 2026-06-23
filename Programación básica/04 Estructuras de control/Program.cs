65a2e2120e8e33e2a59d41ebb9e7fa7273f370e1
Fernando Ruiz <fernando@iessanvicente.com>
2026-06-23 11:23:16 +0200

    Añadir ejemplos de estructuras de control if y if-else en Program.cs

--- Programación básica/04 Estructuras de control/Program.cs
+++ Programación básica/04 Estructuras de control/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Estructura de control if muy simple
+Console.WriteLine("\nIf muy simple:");
+
+int edad = 20;
+
+if (edad >= 18)
+{
+    Console.WriteLine("Eres mayor de edad.");
+    Console.WriteLine("Puedes votar.");
+}
+
+// Estructura de control if-else
+Console.WriteLine("\nIf else muy simple:");
+
+int edad2 = 15;
+
+if (edad2 >= 18)
+{
+    Console.WriteLine("Eres mayor de edad.");
+}
+else
+{
+    Console.WriteLine("Eres menor de edad.");
+}
+
