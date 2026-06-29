698de23f18ab1f0c97ece9f7fef2ea9eb24a4a39
Arturo Bernal <arturober@gmail.com>
2026-06-29 11:11:16 +0200

    Presentación Arrays en C#

--- Programación básica/05 Arrays/Program.cs
+++ Programación básica/05 Arrays/Program.cs
@@ -17,7 +17,7 @@ int[] numeros2 = [12, 23, 53, 5, 92];
 List<int> listaNum = [4, 23, 6, 18, 9, 13];
 
 Console.WriteLine("-- Recorremos con for --");
-for(int i = 0; i < numeros2.Length; i++)
+for (int i = 0; i < numeros2.Length; i++)
 {
   Console.WriteLine(numeros2[i]);
 }
@@ -31,4 +31,35 @@ foreach (var num in numeros2)
 Console.WriteLine("-- Recorremos con Array.ForEach --");
 Array.ForEach(numeros2, n => Console.WriteLine(n));
 
+int sum = 0;
+
+foreach (int n in numeros2)
+{
+  sum += n;
+}
+
+Console.WriteLine($"Suma de numeros: {sum}");
+
+// Buscar valor en array
+string[] nombres = ["Juan", "Ana", "Pedro", "Eva", "Paco"];
+bool encontrado = false; // Nos indicará si hemos encontrado el valor
+string buscar = "Pedro"; // Valor a buscar en el array
+
+// La condición también incluye que no hayamos encontrado lo que buscamos
+for (int i = 0; i < nombres.Length && !encontrado; i++)
+{
+  if (nombres[i] == buscar)
+  {
+    encontrado = true;
+  }
+}
+
+if (encontrado)
+{
+  Console.WriteLine($"El nombre {buscar} está en el array");
+}
+else
+{
+  Console.WriteLine($"{buscar} no encontrado...");
+}
 
