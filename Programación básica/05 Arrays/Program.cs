f87ddba9a31d67fe54e3ce3ccadc7c96d44e15a0
Arturo Bernal <arturober@gmail.com>
2026-06-30 10:09:07 +0200

    arrays multidimensionales

--- Programación básica/05 Arrays/Program.cs
+++ Programación básica/05 Arrays/Program.cs
@@ -1,4 +1,6 @@
-﻿int[] numeros = new int[5];
+﻿using System.Numerics.Tensors;
+
+int[] numeros = new int[5];
 numeros[0] = 15;
 numeros[1] = 3;
 numeros[2] = 8;
@@ -84,10 +86,6 @@ Console.WriteLine(string.Join(", ", palabras));
 Array.Sort(palabras, (p1, p2) => p1.Length - p2.Length);
 Console.WriteLine(string.Join(", ", palabras));
 
-
-var c = Console.ReadKey(true).KeyChar;
-Console.WriteLine(c);
-
 /* EJERCICIO 1 (Parte 2) */
 Console.WriteLine(" --- EJERCICIO 1 (Parte 2) --- ");
 
@@ -108,7 +106,8 @@ while (contador < nombresConsola.Length)
   {
     Console.WriteLine("El nombre ya está repetido");
   }
-  else {
+  else
+  {
     nombresConsola[contador++] = nombre;
   }
 }
@@ -127,14 +126,70 @@ Console.WriteLine(string.Join(',', letras[^3..])); // Las 3 últimas posiciones
 Console.WriteLine(" --- Patrones de lista --- ");
 bool[] interruptores = [true, false, false, true];
 
-if ( interruptores is [true, false, false, true] )
+if (interruptores is [true, false, false, true])
 {
   Console.WriteLine("Los interruptores son correctos");
 }
 
-string texto = """
-  Contenido del archivo
-  con varias líneas.
-  """;
+/* Operador Spread */
+Console.WriteLine(" --- Operador Spread --- ");
 
-var file = new System.IO.StreamReader("archivo.txt");
+string[] frutas = ["Piña", "Melón", "Plátano"];
+string[] frutas2 = ["Manzana", "Pera", "Sandía"];
+
+// Array que contiene los elementos de frutas + frutas2 + otra fruta
+string[] frutas3 = [.. frutas, "Ciruela", .. frutas2];
+Console.WriteLine(string.Join(", ", frutas3));
+
+// Otra forma más clásica de hacerlo
+Console.WriteLine(string.Join(", ", frutas.Append("Ciruela").Concat(frutas2)));
+
+/** --- Ejercicio 7 - Parte 1 --- **/
+Console.WriteLine("--- Ejercicio 7 - Parte 2 ---");
+
+int[] nums1 = [2, 54, 15, 6, 37, 87, 24];
+int[] nums2 = [7, 23, 15, 87, 23, 38, 7];
+
+int[] nums3 = new int[nums1.Length];
+
+for(int i = 0; i < nums1.Length; i++)
+{
+  nums3[i] = nums1[i] + nums2[i];
+}
+
+Console.WriteLine(string.Join(", ", nums3));
+
+// Alternativa con Linq (todavía no lo hemos visto)
+int[] nums4 = nums1.Zip(nums2, (n1, n2) => n1 + n2).ToArray();
+Console.WriteLine(string.Join(", ", nums4));
+
+// Alternativa con TensorPrimitives (operaciones en paralelo)
+int[] nums5 = new int[nums1.Length];
+TensorPrimitives.Add(nums1, nums2, nums5);
+Console.WriteLine(string.Join(", ", nums5));
+
+/*** Ejercicio 3 - Parte 1 ***/
+
+int suma = 0;
+foreach (var n in nums1)
+{
+  suma += n;
+}
+
+double media = suma / (double) nums1.Length;
+
+Console.WriteLine($"Media {media:F2}");
+Console.Write("Números mayores: ");
+foreach (var n in nums1)
+{
+  if(n > media)
+  {
+    Console.Write(n + " ");
+  }
+}
+Console.WriteLine();
+
+// Forma moderna/avanzada
+// double media = nums1.Average();
+// Console.WriteLine($"Media {media:F2}");
+// Console.WriteLine($"Números mayores: {string.Join(", ", nums1.Where(n => n > media))}");
---
+++ Programación básica/06 Arrays Multidimensionales/Program.cs
@@ -0,0 +1,43 @@
+﻿int[,] matriz = {{2,3,4}, {2,1,6}, {3,6,1}, {2,6,8}};
+
+for(int i = 0; i < matriz.GetLength(0); i++) // Recorre filas
+{
+  for(int j = 0; j < matriz.GetLength(1); j++) // Recorre columnas
+  {
+    Console.Write(matriz[i,j] + " ");
+  }
+  Console.WriteLine();
+}
+
+int[][] matriz2 = [[2,3,4], [2,1], [3,6,1], [2,6,8,9]];
+
+for(int i = 0; i < matriz2.Length; i++) // Recorre filas
+{
+  for(int j = 0; j < matriz2[i].Length; j++) // Recorre columnas
+  {
+    Console.Write(matriz2[i][j] + " ");
+  }
+  Console.WriteLine();
+}
+
+/*** Ejercicio 4 - Parte 2 ***/
+Console.WriteLine("Ejercicio 4 - Parte 2");
+
+string[] nombres = ["Juan", "Pepe", "Marta"];
+double[][] notas = [[2, 5.4, 7.6, 8], [3.5, 6.8, 9.4], [7.6, 9, 5.5, 6, 7.4]];
+
+for(int i = 0; i < nombres.Length; i++)
+{
+  double media = 0;
+  for(int j = 0; j < notas[i].Length; j++)
+  {
+    media += notas[i][j];
+  }
+  media /= notas[i].Length;
+
+  Console.WriteLine($"{nombres[i]} - Media: {media:F2}");
+}
+
+
+
+
