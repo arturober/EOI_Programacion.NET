f1e12693cc24ed1dc87f4ec51e919c0650bf75b0
Arturo Bernal <arturober@gmail.com>
2026-07-07 13:01:17 +0200

    diccionarios

---
+++ POO/12 Diccionarios/Program.cs
@@ -0,0 +1,25 @@
+﻿var traducciones = new Dictionary<string, string>
+{
+  { "silla", "chair" },
+  { "mesa", "table" },
+  { "puerta", "door" },
+  { "casa", "house" }
+};
+
+traducciones.Add("ordenador", "computer");
+
+Console.WriteLine(traducciones.Count); // 5
+Console.WriteLine(traducciones["mesa"]); // table
+traducciones["ordenador"] = "laptop"; // Modificar valor
+// traducciones.Add("ordenador", "computer"); // ERROR: An item with the same key has already been added. Key: ordenador
+Console.WriteLine(string.Join(", ", traducciones));
+
+foreach (var palabra in traducciones.Keys)
+{
+  Console.WriteLine($"{palabra} -> {traducciones[palabra]}");
+}
+
+Console.WriteLine(traducciones.ContainsKey("silla"));
+System.Console.Write("Palabra a traducir: ");
+string buscar = Console.ReadLine()!;
+Console.WriteLine(traducciones.GetValueOrDefault(buscar, "Palabra no encontrada"));
