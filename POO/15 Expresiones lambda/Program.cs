e9dbfed3f1079c3c17b18c7f78aa91f7a51ad018
Arturo Bernal <arturober@gmail.com>
2026-07-08 11:39:26 +0200

    linq

---
+++ POO/15 Expresiones lambda/Program.cs
@@ -0,0 +1,65 @@
+﻿void HacerOperacion(double n1, double n2, OperaNums operacion)
+{
+  Console.WriteLine(operacion(n1, n2));
+}
+
+OperaNums sumar = (n1, n2) => n1 + n2;
+OperaNums restar = (n1, n2) => n1 - n2;
+OperaNums multiplicar = (n1, n2) => n1 * n2;
+OperaNums dividir = (n1, n2) => n1 / n2;
+
+Console.WriteLine(sumar(3.5, 6.25));
+
+HacerOperacion(24.4, 6.34, restar);
+HacerOperacion(24.4, 6.34, multiplicar);
+HacerOperacion(24.4, 6.34, dividir);
+
+//------------------
+Func<int, int, int> sumarFunc = (n1, n2) => n1 + n2;
+var sumarFunc2 = (int n1, int n2) => n1 + n2; // Hace lo mismo
+
+Predicate<int> mayor18 = (edad) => edad >= 18;
+
+Action<string> imprimeString = (dato) => Console.WriteLine(dato);
+
+Console.WriteLine(sumarFunc(4, 7));
+Console.WriteLine(mayor18(54));
+
+List<int> edades = [23, 15, 65, 28, 23, 16];
+edades.RemoveAll(mayor18);
+Console.WriteLine(string.Join(", ", edades)); // 15, 16
+
+// La función recibe una lista de enteros y un número
+Action<List<int>, int> numDivisibles = (lista, divisor) =>
+{
+  int veces = lista.FindAll(n => n % divisor == 0).Count;
+  Console.WriteLine($"Hay {veces} números divisibles entre {divisor}");
+};
+
+List<int> numeros = [16, 36, 15, 86, 72, 104, 205, 115];
+numDivisibles(numeros, 3); // Hay 3 números divisibles entre 3
+
+// Predicado
+
+Predicate<Persona> mayorEdad = p => p.Edad >= 18;
+var pepe = new Persona("Pepito", 23);
+Console.WriteLine(mayorEdad(pepe)); // True
+
+// Función
+
+Func<int, int, int> multiplica = (n1, n2) => n1 * n2;
+Func<string, string, int> difLongitud = (s1, s2) => Math.Abs(s1.Length - s2.Length);
+Func<Cuadrado, Cuadrado, Cuadrado> sumaCuadrados = (c1, c2) => new Cuadrado(c1.Lado + c2.Lado);
+
+Console.WriteLine(multiplica(2, 5)); // 10
+Console.WriteLine(difLongitud("caracola", "cebra")); // 3
+
+var cu1 = new Cuadrado(4);
+var cu2 = new Cuadrado(6);
+var sumaCu = sumaCuadrados(cu1, cu2);
+Console.WriteLine($"Lado: {sumaCu.Lado}, área: {sumaCu.Area}");
+
+// El tipo delegado define una función
+delegate double OperaNums(double n1, double n2);
+
+
---
+++ POO/16 Consultas Linq/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> nums = [3, 5, 6, 12, 7, 3, 8, 9, 23, 54];
+List<int> pares = (from n in nums where n % 2 == 0 select n).ToList();
+List<int> pares2 = nums.Where(n => n % 2 == 0).ToList();
+
+int[] arrayNums = [3, 5, 6, 12, 7, 3, 8, 9, 23, 54];
+
+// Los métodos Linq son comunes a todas las colecciones
+Console.WriteLine(nums.All(n => n < 100));
+Console.WriteLine(arrayNums.All(n => n < 100));
+
+var masNums = nums.Concat([1, 2]);
+List<int> masNums2 = [..nums, 1, 2];
+Console.WriteLine(string.Join(",", masNums));
+Console.WriteLine(string.Join(",", masNums2));
+
+
