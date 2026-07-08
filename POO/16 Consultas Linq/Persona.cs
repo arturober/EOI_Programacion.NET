edbaf0e92b8faa0f741238905dac80ee86621861
Arturo Bernal <arturober@gmail.com>
2026-07-08 12:11:07 +0200

    linq

---
+++ POO/16 Consultas Linq/Persona.cs
@@ -0,0 +1,19 @@
+class Persona(string nombre, int edad)
+{
+  public string Nombre { get; set; } = nombre;
+  public int Edad { get; set; } = edad;
+
+  public override int GetHashCode() =>  Tuple.Create(Nombre, Edad).GetHashCode();
+
+  public override bool Equals(object? obj)
+  {
+    if(obj == null || obj is not Persona) return false;
+    var p = (Persona)obj;
+    return p.Nombre == Nombre && p.Edad == Edad;
+  }
+
+  public override string ToString()
+  {
+    return $"{Nombre} ({Edad})";
+  }
+}
--- POO/16 Consultas Linq/Program.cs
+++ POO/16 Consultas Linq/Program.cs
@@ -10,7 +10,62 @@ Console.WriteLine(arrayNums.All(n => n < 100));
 
 var masNums = nums.Concat([1, 2]);
 List<int> masNums2 = [..nums, 1, 2];
-Console.WriteLine(string.Join(",", masNums));
-Console.WriteLine(string.Join(",", masNums2));
+Console.WriteLine(string.Join(",", masNums)); // 3,5,6,12,7,3,8,9,23,54,1,2
+Console.WriteLine(string.Join(",", masNums2)); // 3,5,6,12,7,3,8,9,23,54,1,2
 
+Console.WriteLine(nums.Average()); // 13
+Console.WriteLine(string.Join(",", nums.Distinct())); // 3,5,6,12,7,8,9,23,54
 
+var contarPares = nums.CountBy(n => n % 2 == 0);
+foreach(var pair in contarPares)
+{
+  Console.WriteLine($"{(pair.Key ? "Pares" : "Impares")}: {pair.Value}");
+}
+
+Console.WriteLine(nums.First()); // 3
+Console.WriteLine(nums.First(n => n % 2 == 0)); // 6
+
+Console.WriteLine(nums.Max()); // 54
+
+var numeros = Enumerable.Range(1, 10);
+Console.WriteLine(string.Join(",", numeros));
+
+var unos = Enumerable.Repeat(1, 10);
+Console.WriteLine(string.Join(",", unos));
+
+Console.WriteLine("Colección con 10 números aleatorios");
+var aleatorios = Enumerable.Repeat(0, 10).Select(n => new Random().Next() % 100);
+Console.WriteLine(string.Join(",", aleatorios));
+
+// Ejemplos con personas
+Console.WriteLine("-------- Ejemplos con personas -----");
+
+var personas = new List<Persona>{
+  new("Pepe", 23),
+  new("Juan", 43),
+  new("Ana", 14),
+  new("Paco", 58),
+  new("María", 37),
+  new("Carlos", 16),
+  new("María", 36),
+  new("Juan", 32),
+};
+
+Console.WriteLine($"Media edades: {personas.Average(p => p.Edad):F2}");
+var contarNombres = personas.CountBy(p => p.Nombre);
+foreach(var pair in contarNombres)
+{
+  Console.WriteLine($"{pair.Key} aparece {pair.Value} veces");
+}
+
+Console.WriteLine(personas.Max(p => p.Edad)); // 58
+
+var comparadorEdad = Comparer<Persona>.Create((p1, p2) => p1.Edad.CompareTo(p2.Edad));
+var personasOrdenadas = personas.Order(comparadorEdad);
+Console.WriteLine(string.Join(" - ", personasOrdenadas));
+
+var personasOrdenadas2 = personas.OrderBy(p => p.Edad);
+Console.WriteLine(string.Join(" - ", personasOrdenadas2));
+
+var listaNombres = personas.Select(p => p.Nombre);
+Console.WriteLine(string.Join(", ", listaNombres));
