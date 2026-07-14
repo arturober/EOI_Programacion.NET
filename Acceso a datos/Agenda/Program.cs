3119f9ba9614e5d867b9a14bdf68c671973cba7c
Fernando Ruiz <fernando@iessanvicente.com>
2026-07-14 10:08:50 +0200

    Agregar implementación de la agenda telefónica con creación de tabla en SQLite.

--- Acceso a datos/Agenda/Program.cs
+++ Acceso a datos/Agenda/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("AGENDA TELEFÓNICA");
+﻿using Microsoft.Data.Sqlite;
+
+class ProgramaAgenda
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        string cadenaConexion = "Data Source=agenda.db";
+        SqliteConnection conexion = new SqliteConnection(cadenaConexion);
+
+        using (conexion)
+        {
+            conexion.Open();
+
+            CrearTabla(conexion);
+        }
+    }
+
+    static void CrearTabla(SqliteConnection conexion)
+    {
+        string sqlCrearTabla = 
+                "CREATE TABLE IF NOT EXISTS personas (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nombre TEXT NOT NULL, " +
+                "telefono TEXT NOT NULL)";
+
+        using (SqliteCommand comando = new SqliteCommand(sqlCrearTabla, conexion))
+        {
+            comando.ExecuteNonQuery();
+        }
+    }
+}
