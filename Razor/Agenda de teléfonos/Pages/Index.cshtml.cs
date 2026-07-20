53e03ac3dc2d6ef8aab172d875426d0ca7531cf6
Fernando Ruiz <fernando@iessanvicente.com>
2026-07-20 11:47:47 +0200

    Esqueleto básico de aplicación con Razor Pages

---
+++ Razor/Agenda de teléfonos/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Agenda_de_teléfonos.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ Razor/Agenda de teléfonos/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
