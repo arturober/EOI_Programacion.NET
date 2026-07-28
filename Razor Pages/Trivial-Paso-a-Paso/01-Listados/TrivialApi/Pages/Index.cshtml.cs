e338716c31b2be33c7079e7928d4582b3432dc58
Fernando Ruiz <fernando@iessanvicente.com>
2026-07-28 10:23:23 +0200

    Implementación inicial de la aplicación TrivialApi con estructura de Razor Pages, incluyendo modelos de datos para Categoría y Pregunta, y configuración de contexto de base de datos.

---
+++ Razor Pages/Trivial-Paso-a-Paso/01-Listados/TrivialApi/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using TrivialApi.Data;
+
+namespace TrivialApi.Pages;
+
+public class IndexModel(TrivialContext context) : PageModel
+{
+    
+}
