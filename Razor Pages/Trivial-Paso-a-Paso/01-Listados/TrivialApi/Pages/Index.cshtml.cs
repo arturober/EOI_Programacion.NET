bd2aea6b7cda9cb666b7a80f8dfbe68f0be639c9
Fernando Ruiz <fernando@iessanvicente.com>
2026-07-28 10:28:14 +0200

    Actualiza la página de inicio de TrivialApi con un mensaje de bienvenida y muestra el total de preguntas y categorías disponibles.

--- Razor Pages/Trivial-Paso-a-Paso/01-Listados/TrivialApi/Pages/Index.cshtml.cs
+++ Razor Pages/Trivial-Paso-a-Paso/01-Listados/TrivialApi/Pages/Index.cshtml.cs
@@ -6,5 +6,12 @@ namespace TrivialApi.Pages;
 
 public class IndexModel(TrivialContext context) : PageModel
 {
-    
+    public int TotalPreguntas { get; set; }
+    public int TotalCategorias { get; set; }
+
+    public async Task OnGetAsync()
+    {
+        TotalPreguntas = await context.Preguntas.CountAsync();
+        TotalCategorias = await context.Categorias.CountAsync();
+    }
 }
