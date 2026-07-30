d477ebff039d0d72950c8f2531b355d9a6be7384
Fernando Ruiz <fernando@iessanvicente.com>
2026-07-30 10:26:56 +0200

    Modificaciones cliente js trivial

--- Razor Pages/Trivial-Paso-a-Paso/06-Cliente-Juego/TrivialApi/wwwroot/cliente/trivial.js
+++ Razor Pages/Trivial-Paso-a-Paso/06-Cliente-Juego/TrivialApi/wwwroot/cliente/trivial.js
@@ -5,6 +5,11 @@ let posicion = 0;
 let aciertos = 0;
 
 const selectorCategorias = document.getElementById("categoria");
+const inicio = document.getElementById("inicio");
+const juego = document.getElementById("juego");
+const progreso = document.getElementById("progreso");
+const enunciado = document.getElementById("enunciado");
+const respuestas = document.getElementById("respuestas");
 
 async function obtenerJson(direccion) {
     const respuesta = await fetch(`${api}/${direccion}`);
@@ -27,6 +32,41 @@ async function cargarCategorias() {
         selectorCategorias.appendChild(opcion);
     });
 }
+
+async function empezar() {
+    const categoriaId = selectorCategorias.value;
+    // https://trivial.runasp.net/api/preguntas?categoriaId=1&cantidad=100
+    preguntas = await obtenerJson(`preguntas?categoriaId=${categoriaId}&cantidad=10`);
+
+    console.log(preguntas);
+
+    inicio.classList.add("d-none");
+    juego.classList.remove("d-none");
+
+    posicion = 0;
+    aciertos = 0;
+    mostrarPregunta();
+}
+
+function mostrarPregunta() {
+    const pregunta = preguntas[posicion];
+    console.log(pregunta);
+
+    progreso.textContent = `Pregunta ${posicion + 1} de ${preguntas.length}`;
+
+    enunciado.textContent = pregunta.enunciado;
+
+    respuestas.innerHTML = "";
+
+    pregunta.respuestas.forEach(respuesta => {
+        const boton = document.createElement("button");
+        boton.classList.add("btn", "btn-outline-primary", "d-block", "w-100", "mb-2");
+        boton.textContent = respuesta;
+        respuestas.append(boton);
+    });
+}
+
+document.getElementById("empezar").addEventListener("click", empezar);
 
 cargarCategorias().catch(error => {
     console.error("Error al cargar categorías:", error);
